54c2b544768473819ea5a9260407446c5c5bbecb
HeitorFernando <37213544+HeitorFernando@users.noreply.github.com>
2018-10-30 15:52:24 -0300

    Update Fachada.java

--- src/heitor/Fachada.java
+++ src/heitor/Fachada.java
@@ -1,1 +1,10 @@
-
+public class Fachada {
+  private static Fachada instance= null
+  private Fachada(){
+    public static void getInstance(){
+      if (instance== null){
+        this.instance=new Fachada();
+      }
+    }return instance;
+  }
+}
